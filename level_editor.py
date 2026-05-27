5f438e8cf09801fd8e70e8fc1285422d88965f6a
shizuku002 <k024g0037@g.neec.ac.jp>
2026-05-27 12:33:37 +0900

    Update level_editor.py
    kan

---
+++ level_editor.py
@@ -0,0 +1,22 @@
+import bpy
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "rikuri isobe",
+    "version": (1,0),
+    "blender": (3,3,21),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object",
+}
+
+def register():
+    print("レベルエディタが有効化されました。")
+    
+def unregister():
+    print("レベルエディタが無効化されました。")
+    
+if __name__ == "__main__":
+    register()
